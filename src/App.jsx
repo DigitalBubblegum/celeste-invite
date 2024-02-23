3d52a0ace1b9a675a54de360192d09c87b08124a
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-02-23 14:26:02 +0530

    -added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,33 @@ function App() {
   const [mem, setMem] = useState([])
   const undoFunction = () =>{
     // console.log(mem[mem.length-1])
-    setText(mem[mem.length-1])
-    setMem([...mem.filter((_, index) => index !== mem.length - 1)])
+    if(mem.length!==0){
+      setText(mem[mem.length-1])
+      setMem([...mem.filter((_, index) => index !== mem.length - 1)])
+    }
+  }
+  const clearFunction = () =>{
+    setText('')
   }
   return (
-    <>
-      <h1>Celeste Invite</h1>
-      {text}
+    <div className='bg-gray-100 h-screen w-screen'>
+      <h1 className='text-3xl font-bold p-14 text-center text-indigo-800'>Celeste <span className='text-black'>Invite</span></h1>
+      <div className='flex justify-center items-center'>
+        <div className='p-5 border-2 border-indigo-500 border-dashed'>
+          {text}
+        </div>
+      </div>
       <Greeting text={text} setText={setText} mem={mem} setMem = {setMem}/>
-      <button onClick={undoFunction}>undo</button>
-      <button>redo</button>
-    </>
+      <div className='flex justify-center items-center p-6'>
+        <div className='px-4 bg-indigo-700 rounded-full py-2'>
+          <button onClick={undoFunction}>undo</button>
+        </div>
+        <div className='px-4'></div>
+        <div className='px-4 bg-indigo-700 rounded-full py-2'>
+          <button onClick={clearFunction}>clear</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -10,8 +10,8 @@ const Greeting = ({text,setText,mem,setMem}) =>{
     }
     return(
     <div>
-        <form onSubmit={handleTextSubmit}>
-            <input name ='text' value={text} onChange={handleTextChange} onClick={()=>setText('')}></input>
+        <form className='flex justify-center items-center p-5' onSubmit={handleTextSubmit}>
+            <input className='bg-gray-100 rounded-full border-2 border-indigo-800 shadow-lg px-16 py-4' name ='text' value={text} onChange={handleTextChange} onClick={()=>setText('')}></input>
         </form>
     </div>)
 }
